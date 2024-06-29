2552783c3c9137f7e0d1281a8ae989bff66cc11a
inaki321 <inakihe00@gmail.com>
2024-06-28 19:26:30 -0600

    add clusterip too to loginserver py

--- codeHelpers/loginmainserver.py
+++ codeHelpers/loginmainserver.py
@@ -13,6 +13,9 @@ import sys
 user = sys.argv[1]
 url = "http://main-server.local/login/user/"+user
 print('calling '+url)
+
+clusterIPUrl ="http://10.152.183.100:5000/login/user/" #url for main server using static cluster IP
+
 response = requests.request("GET", url)
 
 print('Response from main server: ')
