d2af7aaef0dd5c5243f650176d718e239368c990
Iñaki Esquivel <inakihe00@gmail.com>
2025-04-23 17:08:24 -0600

    remove k8s folders, just keep helm

--- microservice/dockerImg.sh
+++
@@ -1,5 +0,0 @@
-sudo docker ps
-sudo docker build -t microservice .
-sudo docker tag microservice localhost:32000/microservice
-sudo docker push localhost:32000/microservice
-curl http://localhost:32000/v2/_catalog
--- orchestrator/dockerImg.sh
+++
@@ -1,5 +0,0 @@
-sudo docker ps
-sudo docker build -t orchestrator .
-sudo docker tag orchestrator localhost:32000/orchestrator
-sudo docker push localhost:32000/orchestrator
-curl http://localhost:32000/v2/_catalog
